2cf30cef2e003da3d5c7aaafef547d092ea8fb48
PLeshy <simonegbonifacio@gmail.com>
2026-03-26 18:53:32 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -153,6 +153,7 @@ function AppRoutes() {
             <Route path="payments" element={<ClientPayments />} />
             <Route path="reviews" element={<ClientReview />} />
             <Route path="notifications" element={<ClientNotifications />} />
+            <Route path="messages" element={<ClientMessages />} />
             <Route path="profile" element={<ClientProfile />} />
             <Route path="*" element={<NotFoundPage />} />
           </Route>
